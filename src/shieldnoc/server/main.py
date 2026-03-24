c1731cf5957763bc508bb0cb17b2b30470acfc09
sagiZ3 <sagizar444@gmail.com>
2026-03-24 05:58:43 +0200

    feat: use ChatManager for serving the gui

--- src/shieldnoc/server/main.py
+++ src/shieldnoc/server/main.py
@@ -4,10 +4,6 @@ from shieldnoc.server.managers.vpn import VPNManager
 
 
 def main():
-    gui_main()
-    vpn = VPNManager('wg0', 'eth0', 'path')  # change path
-    # remember to run the vpn model BEFORE the chat model because differently it can cause socket problems
-
     chat_manager = ChatManager()
     chat_manager.start_chat()
     gui_main(chat_manager)  # add a thread! it stuck after
