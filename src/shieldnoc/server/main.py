02fbc6e7684dae4f820934c83a431fcc2803d8ba
sagiZ3 <sagizar444@gmail.com>
2026-03-24 02:39:38 +0200

    docs: important comment for chronological order use of the models

--- src/shieldnoc/server/main.py
+++ src/shieldnoc/server/main.py
@@ -2,6 +2,9 @@ from shieldnoc.server.gui.main import gui_main
 
 def main():
     gui_main()
+    vpn = VPNManager('wg0', 'eth0', 'path')  # change path
+    # remember to run the vpn model BEFORE the chat model because differently it can cause socket problems
+
 
 
 if __name__ == '__main__':
