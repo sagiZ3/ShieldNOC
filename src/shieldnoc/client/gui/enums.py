a2569293eafca7b8ff3b73dcc3638bea00e14a3e
sagiZ3 <sagizar444@gmail.com>
2026-03-30 16:53:29 +0300

    fix(paths): 2 previous paths added for src package to be working directory (paths work from terminal run now & pycharm working directory needs to be edited)

--- src/shieldnoc/client/gui/enums.py
+++ src/shieldnoc/client/gui/enums.py
@@ -1,14 +1,14 @@
 from enum import Enum
 
 class ImagesPaths(Enum):
-    LOGO = 'gui/assets/ShieldNOC_logo.png'
+    LOGO = 'shieldnoc/client/gui/assets/ShieldNOC_logo.png'
 
-    BACKGROUND1 = 'gui/assets/backgrounds/1.png'
-    BACKGROUND2 = 'gui/assets/backgrounds/2.png'
-    BACKGROUND3 = 'gui/assets/backgrounds/3.png'
-    BACKGROUND4 = 'gui/assets/backgrounds/4.png'
-    BACKGROUND5 = 'gui/assets/backgrounds/5.png'
-    BACKGROUND6 = 'gui/assets/backgrounds/6.png'
+    BACKGROUND1 = 'shieldnoc/client/gui/assets/backgrounds/1.png'
+    BACKGROUND2 = 'shieldnoc/client/gui/assets/backgrounds/2.png'
+    BACKGROUND3 = 'shieldnoc/client/gui/assets/backgrounds/3.png'
+    BACKGROUND4 = 'shieldnoc/client/gui/assets/backgrounds/4.png'
+    BACKGROUND5 = 'shieldnoc/client/gui/assets/backgrounds/5.png'
+    BACKGROUND6 = 'shieldnoc/client/gui/assets/backgrounds/6.png'
 
 class TrafficChart(Enum):
     TIME_WINDOW_SECONDS = 120  # 2 minutes window
--- src/shieldnoc/server/gui/enums.py
+++ src/shieldnoc/server/gui/enums.py
@@ -1,17 +1,17 @@
 from enum import Enum
 
 class ImagesPaths(Enum):
-    LOGO = 'gui/assets/ShieldNOC_logo.png'
+    LOGO = 'shieldnoc/server/gui/assets/ShieldNOC_logo.png'
 
-    BACKGROUND1 = 'gui/assets/backgrounds/1.png'
-    BACKGROUND2 = 'gui/assets/backgrounds/2.png'
-    BACKGROUND3 = 'gui/assets/backgrounds/3.png'
-    BACKGROUND4 = 'gui/assets/backgrounds/4.png'
-    BACKGROUND5 = 'gui/assets/backgrounds/5.png'
-    BACKGROUND6 = 'gui/assets/backgrounds/6.png'
+    BACKGROUND1 = 'shieldnoc/server/gui/assets/backgrounds/1.png'
+    BACKGROUND2 = 'shieldnoc/server/gui/assets/backgrounds/2.png'
+    BACKGROUND3 = 'shieldnoc/server/gui/assets/backgrounds/3.png'
+    BACKGROUND4 = 'shieldnoc/server/gui/assets/backgrounds/4.png'
+    BACKGROUND5 = 'shieldnoc/server/gui/assets/backgrounds/5.png'
+    BACKGROUND6 = 'shieldnoc/server/gui/assets/backgrounds/6.png'
 
-    TOPOLOGY_SERVER = 'gui/assets/topology/server.png'
-    TOPOLOGY_CLIENT = 'gui/assets/topology/pc.png'
+    TOPOLOGY_SERVER = 'shieldnoc/server/gui/assets/topology/server.png'
+    TOPOLOGY_CLIENT = 'shieldnoc/server/gui/assets/topology/pc.png'
 
 class TrafficChart(Enum):
     TIME_WINDOW_SECONDS = 120  # 2 minutes window
