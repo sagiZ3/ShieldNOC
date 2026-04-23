6a08ff401482367ab9945769481edb548ea9f69d
sagiZ3 <sagizar444@gmail.com>
2026-04-24 02:11:44 +0300

    feat(vpn): add WireGuard server setup and peer management

--- src/shieldnoc/server/managers/vpn/vpn.py
+++ src/shieldnoc/server/managers/vpn/vpn.py
@@ -1,12 +1,26 @@
 import subprocess
 
+from pathlib import Path
+from random import randint
+
 from shieldnoc.logging_config import logger
 
 
 class VPNManager:
-    def __init__(self, wg_interface: str, lan_interface: str):
-        self._wg_interface = wg_interface
-        self._lan_interface = lan_interface
+    def __init__(self):
+        self._wg_interface = "ShieldNOC"
+        self._lan_interface = self._get_lan_interface()
+
+        self._vpn_ip_prefix = "10.33.33"
+        self._vpn_listen_port = "12345"
+        self._conf_file_path = self._wg_interface + ".conf"
+
+    def _get_lan_interface(self):
+        result = self._run_terminal_cmd(
+            ["ip", "route", "get", "8.8.8.8"],
+            capture_output=True
+        )
+        return result.split("dev")[1].split()[0]
 
     def _enable_ip_forwarding(self) -> None:
         self._run_terminal_cmd(["sysctl", "-w", "net.ipv4.ip_forward=1"])  # kernel level
@@ -36,26 +50,24 @@ class VPNManager:
         except Exception as e:
             logger.error("Error while deleting the vpn rules:", e)
 
-    @staticmethod
-    def is_ip_forwarding_enabled() -> bool:
-        result = subprocess.run(
+    def is_ip_forwarding_enabled(self) -> bool:
+        result = self._run_terminal_cmd(
             ["sysctl", "net.ipv4.ip_forward"],
-            capture_output=True,
-            text=True
+            capture_output=True
         )
-        return " = 1" in result.stdout
+        return " = 1" in result
 
     def is_nat_enabled(self) -> bool:
-        result = subprocess.run(
+        result = self._run_terminal_cmd(
             ["iptables", "-t", "nat", "-S"],
-            capture_output=True,
-            text=True
+            capture_output=True
         )
-        return f"-o {self._lan_interface} -j MASQUERADE" in result.stdout
+        return f"-o {self._lan_interface} -j MASQUERADE" in result
 
-    def setup_server_networking(self) -> bool:
+    def start_vpn(self) -> bool:
         self._enable_ip_forwarding()
         self._enable_forwarding_and_nat_rules()
+        self._start_wg_interface()
 
         if not self.is_ip_forwarding_enabled():
             logger.error("IP forwarding is not enabled!")
@@ -68,7 +80,7 @@ class VPNManager:
         logger.info("~ Server networking configured successfully! ~")
         return True
 
-    def teardown_server_networking(self) -> bool:
+    def stop_vpn(self) -> bool:
         self._disable_forwarding_and_nat_rules()
         self._disable_ip_forwarding()
 
@@ -83,38 +95,78 @@ class VPNManager:
         logger.info("~ Server networking teardown successfully! ~")
         return True
 
-    def load_config(self) -> None:
-        pass
+    def _create_config(self) -> None:
+        config_content = f"""[Interface]
+        PrivateKey = {self.get_private_key()}
+        Address = {self._vpn_ip_prefix}.1/24
+        ListenPort = {self._vpn_listen_port}
+        """
 
-    def save_config(self) -> None:
-        pass
+        with open(self._conf_file_path, "w") as conf_file:
+            conf_file.write(config_content)
 
-    def start_vpn(self) -> None:
-        pass
+    def _start_wg_interface(self) -> None:
+        if not Path(self._conf_file_path).exists():
+            self._create_config()
 
-    def stop_vpn(self) -> None:
-        pass
+        self._run_terminal_cmd(["wg-quick", "up", self._conf_file_path])
 
-    def restart_vpn(self) -> None:
-        pass
+    def _stop_wg_interface(self):
+        self._run_terminal_cmd(["wg-quick", "down", self._wg_interface])
 
-    def is_vpn_running(self) -> bool:
-        pass
+    def get_public_key(self) -> str:
+        # check if exits in DB: False - gen key, True - return it
+        if True:
+            return ""
 
-    def get_vpn_status(self) -> dict:
-        pass
+        key =  self._run_terminal_cmd(["wg", "pubkey"], capture_output=True, input=self.get_private_key())
 
-    def add_peer(self) -> None:
-        pass
+        return key
 
-    def remove_peer(self) -> None:
-        pass
+    def get_private_key(self) -> str:
+        # check if exits in DB: False - gen key, True - return it
+        if True:  # change after build the DB
+            return ""
 
-    def list_peers(self) -> list:
-        pass
+        key = self._run_terminal_cmd(["wg", "genkey"], capture_output=True)
+
+        # add to DB
+
+        return key
+
+    def add_peer(self, client_public_key: str) -> None:
+        client_ip = self._get_random_ip()
+        self._run_terminal_cmd(["wg", "set", self._wg_interface, "peer", client_public_key,
+                                "allowed-ips", f"{client_ip}/32"])
+
+        self._add_client_to_db(client_public_key, client_ip)
+
+    def change_peer_ip(self, client_public_key: str, requested_ip: str) -> None:
+        self._run_terminal_cmd(["wg", "set", self._wg_interface, "peer", client_public_key,
+                                "allowed-ips", f"{requested_ip}/32"])
+
+    def remove_peer(self, client_public_key: str) -> None:
+        self._run_terminal_cmd(["wg", "set", self._wg_interface, "peer", client_public_key, "remove"])
+
+    def _is_ip_in_use(self, ip: str) -> bool:
+        pass  # TODO: db check
+
+    def _add_client_to_db(self, public_key ,ip):
+        pass  # TODO: db add
+
+    def _get_random_ip(self) -> str:
+        while True:
+            random_host_ip = str(randint(2, 254))
+            new_ip = f"{self._vpn_ip_prefix}.{random_host_ip}"
+
+            if not self._is_ip_in_use(new_ip):
+                break
+
+        return new_ip
 
     @staticmethod
-    def _run_terminal_cmd(cmd: list[str]) -> None:
-        subprocess.run(cmd, check=True)
+    def _run_terminal_cmd(cmd: list[str], capture_output=False, **kwargs) -> str | None:
+        result = subprocess.run(cmd, check=True, text=True, capture_output=capture_output, **kwargs)
+        return result.stdout.strip() if capture_output else None
 
 # add to client (download button): subprocess.run('winget install --id WireGuard.WireGuard -e --source winget', shell=True)
