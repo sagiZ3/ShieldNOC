499b9dc86ecd83e4a4c9728a5e6d25b2f25a7349
sagiZ3 <sagizar444@gmail.com>
2026-04-28 01:55:35 +0300

    refactor: delete useless fields

--- src/shieldnoc/server/core/db/enums.py
+++ src/shieldnoc/server/core/db/enums.py
@@ -3,11 +3,9 @@ from enum import Enum
 class ClientField(Enum):
     PUB_KEY = "public_key"
     VPN_IP = "vpn_ip"
-    IP = "ip"
     MAC = "mac"
     HOST = "host"
     HOSTNAME = "hostname"
     LAST_SEEN = "last_seen"
     STATUS = "status"
     IP_PREF = "ip_preference"
-    # BG_PREF = "background_preference"  # should be store at the client DB
