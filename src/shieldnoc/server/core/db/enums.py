250aaa8a2fe3faee97c0852ad8770e61d3aa767c
sagiZ3 <sagizar444@gmail.com>
2026-04-28 02:36:30 +0300

    feat: add server fields enum and improve field naming consistency

--- src/shieldnoc/server/core/db/enums.py
+++ src/shieldnoc/server/core/db/enums.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class ClientField(Enum):
-    PUB_KEY = "public_key"
+    PUBLIC_KEY = "public_key"
     VPN_IP = "vpn_ip"
     MAC = "mac"
     HOST = "host"
@@ -9,3 +9,9 @@ class ClientField(Enum):
     LAST_SEEN = "last_seen"
     STATUS = "status"
     IP_PREF = "ip_preference"
+
+class ServerField(Enum):
+    ID = "id"
+    PRIVATE_KEY = "private_key"
+    PUBLIC_KEY = "public_key"
+    CREATED_TIME = "created_time"
