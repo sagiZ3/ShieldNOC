42d53abbd90902919e55315313615d9ccf6d1a47
sagiZ3 <sagizar444@gmail.com>
2026-04-27 17:09:43 +0300

    chore: TODO done - architecture change (server side ready)

--- src/shieldnoc/client/managers/vpn.py
+++ src/shieldnoc/client/managers/vpn.py
@@ -2,8 +2,7 @@ import subprocess
 
 from shieldnoc.logging_config import logger
 
-# TODO: needs to open a socket to the VPNManager with the server side for transporting related information
-#  (e.g. Connected VPN Users <-> Changing the client VPN IP by request
+
 class VPNManager:
     def __init__(self):
         pass
