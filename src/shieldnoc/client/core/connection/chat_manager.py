9f0c1a62aa9989804fe3c980c4866f867acc876c
sagiZ3 <sagizar444@gmail.com>
2026-05-03 15:56:27 +0300

    refactor: simplify chat manager by removing send callback usage and stop event

--- src/shieldnoc/client/core/connection/chat_manager.py
+++ src/shieldnoc/client/core/connection/chat_manager.py
@@ -1,23 +1,15 @@
-from threading import Event
-
-
 class ChatManager:
     def __init__(self, send_msg_callback):
-        self._send_msg = send_msg_callback
+        self.send_msg = send_msg_callback
         self._messages: list = []
-        self.stop_connection_event = Event()
 
     def _add_msg_to_queue(self, msg: str):
         self._messages.append(msg)
 
     def handle_msg(self, msg):
         self._add_msg_to_queue(msg)
-        self._send_msg(msg)
 
     def get_next_msg(self) -> str | None:
         if self._messages:
             return self._messages.pop(0)
         return None
-
-    def end_chat_session(self):
-        self.stop_connection_event.set()
